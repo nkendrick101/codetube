6b7bb75d9bdb08d1a49bb3b2f08e267050b54169
Filemon <filemon.mateus@gmail.com>
2018-07-05 18:02:51 +0100

    codetube v2.0

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -34,7 +34,6 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
-            'activated' => 0
         ]);
 
         $user->channel()->create([
