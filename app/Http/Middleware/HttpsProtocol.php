dff36c572c80395994a7578991dcb178eabf3cb1
Filemon Mateus <fmateus.17@westminster-mo.edu>
2018-11-08 19:29:13 +0100

    last commit, well not really

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -6,19 +6,12 @@ use Closure;
 
 class HttpsProtocol
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        $request->setTrustedProxies( [ $request->getClientIp() ] );
-        if (!$request->secure() && env('APP_ENV') === 'production') {
+    public function handle ($request, Closure $next) {
+        $request->setTrustedProxies([$request->getClientIp()]);
+
+        if (!$request->secure() && env('APP_ENV') === 'production')
             return redirect()->secure($request->getRequestUri());
-        }
+  
         return $next($request); 
     }
 }
