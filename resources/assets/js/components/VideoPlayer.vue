eb679a66f9d9c7ad7d48001b2766554474691ee4
Filemon Mateus <fmateus.17@westminster-mo.edu>
2018-11-09 23:11:44 +0100

    last commit, well not really

--- resources/assets/js/components/VideoPlayer.vue
+++ resources/assets/js/components/VideoPlayer.vue
@@ -7,53 +7,56 @@
 </template>
 
 <script>
-  import { Plyr } from 'vue-plyr';
-  import 'vue-plyr/dist/vue-plyr.css';
+import { Plyr } from "vue-plyr";
+import "vue-plyr/dist/vue-plyr.css";
 
-  export default {
+export default {
     components: {
-      Plyr  		
+        Plyr
     },
 
-    data () {
-      return {
-        player: null,
-        duration: null,
-        token: document.getElementById('csrf-token').getAttribute('content')
-      }
+    data() {
+        return {
+            player: null,
+            duration: null,
+            token: document.getElementById("csrf-token").getAttribute("content")
+        };
     },
 
     props: {
-      videoUuid: null,
-      videoUrl: null,
-      thumbnailUrl: null,
+        videoUuid: null,
+        videoUrl: null,
+        thumbnailUrl: null
     },
 
     methods: {
-      hasHitQuotaView () {
-        if (!this.player.duration) {
-          return false;
+        hasHitQuotaView() {
+            if (!this.player.duration) {
+                return false;
+            }
+
+            return (
+                Math.round(this.player.currentTime) ===
+                Math.round((10 * this.player.duration) / 100)
+            );
+        },
+
+        createView() {
+            this.$http.post("/videos/" + this.videoUuid + "/view", {
+                _token: this.token
+            });
         }
-
-        return Math.round(this.player.currentTime) === Math.round((10 * this.player.duration) / 100);
-      },
-
-      createView () {
-        this.$http.post('/videos/' + this.videoUuid + '/view', {
-          _token: this.token,
-        });
-      },
     },
 
-    mounted () {
-      this.player = this.$refs.player.player;
-      this.player.play();
+    mounted() {
+        this.player = this.$refs.player.player;
+        this.player.play();
 
-      setInterval(() => {
-        if (this.hasHitQuotaView()) {
-          this.createView();
-        }
-      }, 1000);
+        setInterval(() => {
+            if (this.hasHitQuotaView()) {
+                this.createView();
+            }
+        }, 1000);
     }
-  }
+};
 </script>
