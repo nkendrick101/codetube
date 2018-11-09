d52e9cf75b45dcaeab21617da875ad8a4a3e7354
Filemon Mateus <fmateus.17@westminster-mo.edu>
2018-11-09 19:21:32 +0100

    last commit, well not really

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -51,9 +51,10 @@ class LoginController extends Controller
       return redirect('/');
     }
 
-    if (User::where('email', $social_user->email)->count()) {
-      return redirect()->back()->with('status', 'Email address has been already taken.');
-    }
+    // TODO Handle this
+    /*if (User::where('email', $social_user->email)->count()) {
+      return back()->with('notification', 'Email address has been already taken.');
+    }*/
 
     $field = $social_network . '_id';
     $user = User::where($field, $social_user->id)->first();
